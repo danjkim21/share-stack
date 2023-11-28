91b417fdf1b51b07f9a4cc16fc41e7ec62f6902d
danjkim21 <dan.jkim21@gmail.com>
2023-11-28 10:18:45 -0500

    added settings boiler plate component

--- app/(app-ui)/profile/page.tsx
+++ app/(app-ui)/profile/page.tsx
@@ -1,22 +1,13 @@
-'use client';
-
-import { Fragment } from 'react';
 import {
   BriefcaseIcon,
   CalendarIcon,
-  ChevronDownIcon,
   CurrencyDollarIcon,
   LinkIcon,
   MapPinIcon,
   PencilIcon,
 } from '@heroicons/react/20/solid';
-import { Menu, Transition } from '@headlessui/react';
 
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(' ');
-}
-
-export default async function Profile() {
+export default function Profile() {
   return (
     <div className="lg:flex lg:items-center lg:justify-between mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
       <div className="min-w-0 flex-1">
@@ -80,56 +71,6 @@ export default async function Profile() {
             View
           </button>
         </span>
-
-        {/* Dropdown */}
-        <Menu as="div" className="relative ml-3 sm:hidden">
-          <Menu.Button className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400">
-            More
-            <ChevronDownIcon
-              className="-mr-1 ml-1.5 h-5 w-5 text-gray-400"
-              aria-hidden="true"
-            />
-          </Menu.Button>
-
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-200"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items className="absolute right-0 z-10 -mr-1 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active ? 'bg-gray-100' : '',
-                      'block px-4 py-2 text-sm text-gray-700'
-                    )}
-                  >
-                    Edit
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active ? 'bg-gray-100' : '',
-                      'block px-4 py-2 text-sm text-gray-700'
-                    )}
-                  >
-                    View
-                  </a>
-                )}
-              </Menu.Item>
-            </Menu.Items>
-          </Transition>
-        </Menu>
       </div>
     </div>
   );
