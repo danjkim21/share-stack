793aabde0c6763b3c431d25f14e97268351b6928
danjkim21 <dan.jkim21@gmail.com>
2023-12-01 10:55:22 -0500

    update group mock data

--- app/(app-ui)/groups/page.tsx
+++ app/(app-ui)/groups/page.tsx
@@ -15,17 +15,17 @@ const groups = [
           'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Nelly',
         avatarSrc:
           'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Joe',
         avatarSrc:
           'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Shmo',
         avatarSrc:
           'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
@@ -42,17 +42,17 @@ const groups = [
           'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Nelly',
         avatarSrc:
           'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Joe',
         avatarSrc:
           'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Shmo',
         avatarSrc:
           'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
@@ -69,17 +69,17 @@ const groups = [
           'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Nelly',
         avatarSrc:
           'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Joe',
         avatarSrc:
           'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Shmo',
         avatarSrc:
           'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
@@ -96,17 +96,17 @@ const groups = [
           'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Nelly',
         avatarSrc:
           'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Joe',
         avatarSrc:
           'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Shmo',
         avatarSrc:
           'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
@@ -123,17 +123,17 @@ const groups = [
           'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Nelly',
         avatarSrc:
           'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Joe',
         avatarSrc:
           'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Shmo',
         avatarSrc:
           'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
@@ -150,17 +150,17 @@ const groups = [
           'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Nelly',
         avatarSrc:
           'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Joe',
         avatarSrc:
           'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
       },
       {
-        name: 'Bobby',
+        name: 'Shmo',
         avatarSrc:
           'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
--- components/avatar-group/index.tsx
+++ components/avatar-group/index.tsx
@@ -9,6 +9,7 @@ function AvatarGroup({ avatars }: AvatarGroupInterfaceProps) {
         className="inline-block h-6 w-6 rounded-full ring-2 ring-white"
         src={avatar.avatarSrc}
         alt={avatar.name}
+        key={avatar.name}
       />
     );
   });
