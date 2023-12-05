c3c026b826162a8dc3fc78ee43984513fbbd87d9
danjkim21 <dan.jkim21@gmail.com>
2023-12-05 12:37:57 -0500

    added draft command palette

--- app/(app-ui)/dashboard/page.tsx
+++ app/(app-ui)/dashboard/page.tsx
@@ -22,6 +22,16 @@ export default async function Dashboard() {
       <HeaderStacked title="Dashboard" />
       <main>
         <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          {/* Action Prompt */}
+          <div className="fixed bottom-6 right-6">
+            <p className="text-muted-foreground text-sm">
+              Press{' '}
+              <kbd className="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100">
+                <span className="text-xs">⌘</span>J
+              </kbd>
+            </p>
+          </div>
+
           {/* Stats */}
           <div className="bg-white sm:py-4">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
--- app/(app-ui)/layout.tsx
+++ app/(app-ui)/layout.tsx
@@ -1,5 +1,6 @@
 import Nav from '@/components/nav';
 import '../styles/globals.css';
+import CommandMenu from '@/components/command-menu';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -19,6 +20,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <Nav />
           {children}
         </div>
+        <CommandMenu />
       </body>
     </html>
   );
